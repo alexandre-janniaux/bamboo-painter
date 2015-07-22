6e91a04ae8f07c5e5e9c4068e71ffa5efc53a04f
Novatux <nathanael.courant@laposte.net>
2015-07-22 16:23:14 +0200

    Physics, part II: working better

--- src/GameScreenState.cpp
+++ src/GameScreenState.cpp
@@ -12,15 +12,7 @@ GameScreenState::GameScreenState() {
 }
 
 void GameScreenState::event(const sf::Event& event) {
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-		velocity.x = 100;
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-		velocity.x = -100;
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-		if (velocity.y == 0) velocity.y = -100; // FIXME
-	}
+
 }
 
 void GameScreenState::updateSprites() {
@@ -58,33 +50,54 @@ void GameScreenState::render(sf::RenderTarget& target) {
 
 
 void GameScreenState::update(const sf::Time& time) {
+	float s = time.asSeconds();
+	pos = pos + velocity * s;
+	sf::Vector2f oldv = velocity;
+	velocity = velocity + s * acceleration;
 	acceleration = sf::Vector2f(0, 80);
-	rectangle me_ex = rectangle(pos - sf::Vector2f(22, 22), pos + sf::Vector2f(22, 22));
-	rectangle me = rectangle(pos - sf::Vector2f(20, 20), pos + sf::Vector2f(20, 20));
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		velocity.x = 100;
+    } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		velocity.x = -100;
+	} else {
+		velocity.x = 0;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+		if (velocity.y == 0) velocity.y = -100; // FIXME
+	}
+	rectangle me_ex = rectangle(pos - sf::Vector2f(20, 19.9), pos + sf::Vector2f(20, 19.9));
+	rectangle me_ey = rectangle(pos - sf::Vector2f(19.9, 20), pos + sf::Vector2f(19.9, 20));
 	for (unsigned int i = 0; i < level.size(); i++) {
-		if (!level[i].intersects(me_ex)) continue;
-		if (!((level[i].minp.x > me.maxp.x) || (me.minp.x > level[i].maxp.x))) {
-			if (me.minp.x >= level[i].minp.x) {
+		//if (!level[i].intersects(me_ex)) continue;
+		//if (!((level[i].minp.x > me.maxp.x) || (me.minp.x > level[i].maxp.x))) {
+		if (level[i].intersects(me_ex)) {
+			if (me_ey.minp.x >= level[i].minp.x) {
+				if (me_ey.minp.x >= level[i].maxp.x + oldv.x * s)
+					pos.x += std::max(level[i].maxp.x - me_ex.minp.x, (float)0);
 				velocity.x = std::max(velocity.x, (float)0);
 				acceleration.x = std::max(acceleration.x, (float)0);
 			}
-			if (me.maxp.x <= level[i].maxp.x) {
+			if (me_ey.maxp.x <= level[i].maxp.x) {
+				if (me_ey.maxp.x <= level[i].minp.x + oldv.x * s)
+					pos.x += std::min(level[i].minp.x - me_ex.maxp.x, (float)0);
 				velocity.x = std::min(velocity.x, (float)0);
 				acceleration.x = std::min(acceleration.x, (float)0);
 			}
 		}
-		if (!((level[i].minp.y > me.maxp.y) || (me.minp.y > level[i].maxp.y))) {
-			if (me.minp.y >= level[i].minp.y) {
+		//if (!((level[i].minp.y > me.maxp.y) || (me.minp.y > level[i].maxp.y))) {
+		if (level[i].intersects(me_ey)) {
+			if (me_ex.minp.y >= level[i].minp.y) {
+				if (me_ex.minp.y >= level[i].maxp.y + oldv.y * s)
+					pos.y += std::max(level[i].maxp.y - me_ey.minp.y, (float)0);
 				velocity.y = std::max(velocity.y, (float)0);
 				acceleration.y = std::max(acceleration.y, (float)0);
 			}
-			if (me.maxp.y <= level[i].maxp.y) {
+			if (me_ex.maxp.y <= level[i].maxp.y) {
+				if (me_ex.maxp.y <= level[i].minp.y + oldv.y * s)
+					pos.y += std::min(level[i].minp.y - me_ey.maxp.y, (float)0);
 				velocity.y = std::min(velocity.y, (float)0);
 				acceleration.y = std::min(acceleration.y, (float)0);
 			}
 		}
 	}
-	float s = time.asSeconds();
-	pos = pos + velocity * s;
-	velocity = velocity + s * acceleration;
 }
